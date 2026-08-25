b4132d38e312ff1d9851919d54708a42afcd0fff
NoirHare <177549718+NoirHare@users.noreply.github.com>
2026-08-25 10:24:05 +0800

    0

--- packages/nr-agent-presets/src/index.ts
+++ packages/nr-agent-presets/src/index.ts
@@ -2,7 +2,7 @@ import { Context } from "@deepseek-ai/cordis";
 import NRAgentPresets from "@noirhare/dsh-agent-presets";
 import Path from "node:path";
 
-export const inject = [NRAgentPresets.name];
+export const inject = ["agentPresets"];
 
 export function apply(ctx: Context) {
     (ctx.agentPresets as NRAgentPresets).registerRoot(Path.resolve(import.meta.dirname, "agent-presets"));
