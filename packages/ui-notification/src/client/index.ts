1108aa7989208bdb7350f1718fba65af2a008b83
NoirHare <177549718+NoirHare@users.noreply.github.com>
2026-09-14 19:15:33 +0800

    0

--- packages/ui-notification/src/client/index.ts
+++ packages/ui-notification/src/client/index.ts
@@ -4,21 +4,25 @@ import type {} from "@deepseek-ai/dsh-client-ui-renderer/client";
 import type {} from "@deepseek-ai/dsh-client-ui-sidebar/client";
 
 import type {} from "@deepseek-ai/dsh-api-session-controller/client";
+import type {} from "@deepseek-ai/dsh-client-ui-session/client";
 
+import itiriri from "itiriri";
 import SidebarNotify from "./SidebarNotify";
 
 const ID = "@noirhare/dsh-ui-notification";
 
-export const inject = ["slots", "sessions"];
+export const inject = ["slots", "sessions", "uiSession"];
 
 export async function apply(ctx: Context) {
     if (Notification.permission === "default") {
-        ctx.slots.inject(
-            "sidebar.footer.action",
-            () => ctx.slots.register({
-                name: "sidebar.footer.action",
-                id: `${ID}:request-permission`,
-            }, SidebarNotify),
+        ctx.slots.inject("sidebar.footer.action", () =>
+            ctx.slots.register(
+                {
+                    name: "sidebar.footer.action",
+                    id: `${ID}:request-permission`,
+                },
+                SidebarNotify,
+            ),
         );
     }
 
@@ -42,18 +46,6 @@ export async function apply(ctx: Context) {
                             e.preventDefault();
                             ctx.sessions.open(id);
                             window.focus();
-                            // n.close();
-                        });
-                    }
-
-                    // pending
-                    if (p.pendingInteraction !== c.pendingInteraction && c.pendingInteraction && !document.hasFocus()) {
-                        const n = new Notification(c.displayTitle, { body: `${c.pendingInteraction} 等待响应` });
-                        n.addEventListener("click", (e) => {
-                            e.preventDefault();
-                            ctx.sessions.open(id);
-                            window.focus();
-                            // n.close();
                         });
                     }
                 }
@@ -61,5 +53,26 @@ export async function apply(ctx: Context) {
                 previous = snapshot.byId;
             }
         });
-    }, `${ID}:session-update`);
+    }, `${ID}:sessions-update`);
+
+    ctx.effect(() => {
+        let previous = ctx.uiSession.pendingInteractions.getSnapshot().keys();
+        return ctx.uiSession.pendingInteractions.subscribe(() => {
+            const sessions = ctx.sessions.list.getSnapshot().byId;
+            const ids = itiriri(ctx.uiSession.pendingInteractions.getSnapshot().keys()).exclude(previous);
+
+            for (const id of ids) {
+                if (Notification.permission !== "granted") return;
+
+                const c = sessions[id];
+
+                const n = new Notification(c.displayTitle, { body: `等待响应` });
+                n.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    ctx.sessions.open(id);
+                    window.focus();
+                });
+            }
+        });
+    }, `${ID}:interactions-update`);
 }
